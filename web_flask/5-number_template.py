aa12ff81a74b007d0a15a21c291d5da18884cfe3
frenol021 <frenol021@gmail.com>
2023-02-08 18:50:19 +0300

    fixed

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,43 +1,53 @@
 #!/usr/bin/python3
-""" 4. Add fourth view function that displays var only if is integer """
+""" 5. Add fifth view func that displays HTML page if n is int """
 
 from flask import Flask
+from flask import render_template
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-@app.route('/', strict_slashes=False)
+
+@app.route('/')
 def hello_world():
     """ Returns some text. """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hello():
     """ Return other text. """
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<text>')
 def c_text(text):
     """ replace text with variable. """
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
 
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/')
+@app.route('/python/<text>')
 def python_text(text='is cool'):
     """ replace more text with another variable. """
     text = text.replace('_', ' ')
     return 'Python {}'.format(text)
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def isNumber(n):
     """display “n is a number” only if n is an integer"""
     if isinstance(n, int):
         return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n=None):
+    """display a HTML page only if n is an integer"""
+    if isinstance(n, int):
+        return render_template("5-number.html", n=n)
 
 
 if __name__ == '__main__':
