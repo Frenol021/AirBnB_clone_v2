77591ce0d51db8725c9e5f78caf833b881321a40
frenol021 <frenol021@gmail.com>
2023-02-09 02:24:37 +0000

    fixed

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,44 +1,44 @@
 #!/usr/bin/python3
-""" 4. Add fourth view function that displays var only if is integer """
+""" Write a script that starts a Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000
+"""
 
 from flask import Flask
 
+app = Flask("__name__")
 
-app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
-def hello_world():
-    """ Returns some text. """
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb', strict_slashes=False)
 def hello():
-    """ Return other text. """
-    return 'HBNB'
+    """Return a given string"""
+    return ("Hello HBNB!")
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """ replace text with variable. """
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Returns a given string"""
+    return ("HBNB")
 
 
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text='is cool'):
-    """ replace more text with another variable. """
-    text = text.replace('_', ' ')
-    return 'Python {}'.format(text)
+@app.route("/c/<text>", strict_slashes=False)
+def cText(text):
+    """display C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def number_text(n):
-    """ replace with int only if given int. """
-    n = str(n)
-    return '{} is a number'.format(n)
+@app.route('/python', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythonText(text="is cool"):
+    """display Python followed by the value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/number/<int:n>", strict_slashes=False)
+def isNumber(n):
+    """display “n is a number” only if n is an integer"""
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
